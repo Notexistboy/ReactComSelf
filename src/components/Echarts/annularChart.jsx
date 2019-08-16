5d51f3e162df5168ac8b8e663383c621fcca33a6
Seong <seonghan0508@gmail.com>
2019-08-16 17:30:08 +0800

    echarts

--- src/components/Echarts/annularChart.jsx
+++ src/components/Echarts/annularChart.jsx
@@ -9,20 +9,33 @@ export default class fanChart extends Component {
   constructor(props) {
     super(props);
     this.props = {
-      
     };
     this.state = {
-
+      legendData:[],
+      listData: [],
+      seriesData: [],
+      arrayData:[]
     }
   }
-  test = () => {
+  setOption = () => {
     const { test } =this.props;
     console.log(test)
+    return (
+      <div>
+        <ReactEcharts
+            option={chartDatas}
+            lazyUpdate={true}
+            notMerge={true}
+            id="rightLine"
+            style={{ width: 'calc(80vw)', height: 'calc(60vh)' , marginTop: '5vh'}}
+        />
+      </div>
+    )
   }
   render() {
     return (
       <div>
-        {this.test()}
+        {this.setOption()}
       </div>
     )
   }
